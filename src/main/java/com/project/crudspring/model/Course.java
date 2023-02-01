66d73cf60e465f943926a74c3d21ad28c7e37918
Yuri Martins <yurigamer16@gmail.com>
2023-01-31 22:21:30 -0300

    Update pm.xml

--- src/main/java/com/project/crudspring/model/Course.java
+++ src/main/java/com/project/crudspring/model/Course.java
@@ -1,18 +1,14 @@
 package com.project.crudspring.model;
-import lombok.Setter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.Getter;
 
 
 @Data
 @Entity
-//@Table()
 public class Course {
     
     @Id
