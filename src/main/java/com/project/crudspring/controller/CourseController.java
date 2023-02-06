f028ff5c71a0b1b25a15cb07af06763b3b2ad654
Yuri Martins <yurigamer16@gmail.com>
2023-02-05 21:06:59 -0300

    Create Endpoint Create Course

--- src/main/java/com/project/crudspring/controller/CourseController.java
+++ src/main/java/com/project/crudspring/controller/CourseController.java
@@ -1,7 +1,10 @@
 package com.project.crudspring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,11 @@ import com.project.crudspring.model.Course;
 import com.project.crudspring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -18,10 +26,28 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    @GetMapping
+    @GetMapping("list")
     public List<Course> list() {
         return courseRepository.findAll();
     }
+
+    @GetMapping("item")
+    public Optional<Course> getById(@RequestParam("id") Long id) {
+        return courseRepository.findById(id);
+    }
+
+    @PostMapping(value="item")
+    public ResponseEntity<?> createCourse(@RequestBody Course data) {
+        try {
+            courseRepository.save(data);
+            return new ResponseEntity(null, HttpStatus.OK);        
+        } catch (Exception e) {
+            return new ResponseEntity(null, HttpStatus.EXPECTATION_FAILED);  
+        }
+    
+    }
+    
+    
 
     
 }
